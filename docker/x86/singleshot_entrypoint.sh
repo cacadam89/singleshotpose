0915e1f79ea9d7c01eeb8e7c93016cb49ebf1ef8
Adam Caccavale <adam.w.caccavale@gmail.com>
2020-02-03 14:37:20 -0800

    fixed a python path issue preventing cv_bridge from working correctly

--- docker/x86/singleshot_entrypoint.sh
+++ docker/x86/singleshot_entrypoint.sh
@@ -9,10 +9,6 @@ cd /root/ssp_ws/src/singleshotpose
 
 # setup ros environment
 source "/root/ssp_ws/devel/setup.bash"
-# export PYTHONPATH="/root/python3_ws/install/lib/python3/dist-packages"
-# source "/usr/local/bin/nvidia_entrypoint.sh"
-
-# # source ros
-# source ~/ros_catkin_ws/install_isolated/setup.bash
+export PYTHONPATH="$PYTHONPATH:/root/python3_ws/install/lib/python3/dist-packages"
 
 exec "$@"
